5ff46f30e8cba0b6df7a31e414d11a56589ed040
Jason Cairns <jason@cair.nz>
2024-10-28 20:43:20 +1300

    use getCh in 10.6

--- 10.6.hs
+++ 10.6.hs
@@ -1,16 +1,31 @@
+import System.IO
+
+getCh :: IO Char
+getCh = do
+         hSetEcho stdin False
+         x <- getChar
+         hSetEcho stdin True
+         return x
+
 readLine :: IO String
 readLine = readLine' ""
   where
     readLine' :: String -> IO String
     readLine' cs = do
-      c <- getChar
-      if c == '\DEL' then
-        if not (null cs) then do
-          putStr "\b \b"
-          readLine' (init cs)
-        else
-          readLine' cs
-      else if c == '\n' then
-        return cs
-      else
-        readLine' (cs ++ [c])
+      c <- getCh
+      case c of
+
+        '\DEL' ->
+          if (null cs) then
+            readLine' cs
+          else do
+            putStr "\b \b"
+            readLine' (init cs)
+
+        '\n' -> do
+          putChar c
+          return cs
+
+        _    -> do
+          putChar c
+          readLine' (cs ++ [c])

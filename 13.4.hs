d8f06e972ae3c36cb2a15e159370299f41dd9669
Jason Cairns <jason@cair.nz>
2024-11-25 21:34:25 +1300

    13.4

---
+++ 13.4.hs
@@ -0,0 +1,6 @@
+{-
+Because all of the correct options for a single number are on the
+right side of the choice operator, there will be a significant amount
+of backtracking. With the new parser, the only tests that are required
+are for the existence of operators + and *, a lookahead of 1.
+-}
